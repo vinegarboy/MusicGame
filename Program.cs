5435bda7dcd06442fd8668aab55626fcdd2583cc
Vinegarman <necogusa398@gmail.com>
2020-12-28 09:57:27 +0900

    ボタン作成

--- Program.cs
+++ Program.cs
@@ -19,16 +19,55 @@ namespace MusicGame
             if (DX.DxLib_Init() == -1) return;
             while(DX.CheckHitKey(DX.KEY_INPUT_ESCAPE) == 0){
                 DX.ClearDrawScreen();
-                DX.DrawBox(20,(H-W),20+box_size,(H-W)+box_size,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(40+box_size,(H-W),40+box_size*2,(H-W)+box_size,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(60+box_size*2,(H-W),60+box_size*3,(H-W)+box_size,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(20,(H-W)+20+box_size,20+box_size,(H-W)+20+box_size*2,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(40+box_size,(H-W)+20+box_size,40+box_size*2,(H-W)+20+box_size*2,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(60+box_size*2,(H-W)+20+box_size,60+box_size*3,(H-W)+20+box_size*2,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(20,(H-W)+40+box_size*2,20+box_size,(H-W)+40+box_size*3,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(40+box_size,(H-W)+40+box_size*2,40+box_size*2,(H-W)+40+box_size*3,DX.GetColor(180,180,180),DX.TRUE);
-                DX.DrawBox(60+box_size*2,(H-W)+40+box_size*2,60+box_size*3,(H-W)+40+box_size*3,DX.GetColor(180,180,180),DX.TRUE);
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD7)==1||DX.CheckHitKey(DX.KEY_INPUT_Q)==1){
+                    DX.DrawBox(20,(H-W),20+box_size,(H-W)+box_size,DX.GetColor(180,180,180),DX.TRUE);
+                }
+                else{
+                    DX.DrawBox(20,(H-W),20+box_size,(H-W)+box_size,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD8)==1||DX.CheckHitKey(DX.KEY_INPUT_W)==1){
+                    DX.DrawBox(40+box_size,(H-W),40+box_size*2,(H-W)+box_size,DX.GetColor(180,180,180),DX.TRUE);
+                }else{
+                    DX.DrawBox(40+box_size,(H-W),40+box_size*2,(H-W)+box_size,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD9)==1||DX.CheckHitKey(DX.KEY_INPUT_E)==1){
+                    DX.DrawBox(60+box_size*2,(H-W),60+box_size*3,(H-W)+box_size,DX.GetColor(180,180,180),DX.TRUE);
+                }else{
+                    DX.DrawBox(60+box_size*2,(H-W),60+box_size*3,(H-W)+box_size,DX.GetColor(50,50,50),DX.TRUE);
+                }
 
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD4)==1||DX.CheckHitKey(DX.KEY_INPUT_A)==1){
+                    DX.DrawBox(20,(H-W)+20+box_size,20+box_size,(H-W)+20+box_size*2,DX.GetColor(180,180,180),DX.TRUE);
+                }else{
+                    DX.DrawBox(20,(H-W)+20+box_size,20+box_size,(H-W)+20+box_size*2,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD5)==1||DX.CheckHitKey(DX.KEY_INPUT_S)==1){
+                    DX.DrawBox(40+box_size,(H-W)+20+box_size,40+box_size*2,(H-W)+20+box_size*2,DX.GetColor(180,180,180),DX.TRUE);
+                }
+                else{
+                    DX.DrawBox(40+box_size,(H-W)+20+box_size,40+box_size*2,(H-W)+20+box_size*2,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD6)==1||DX.CheckHitKey(DX.KEY_INPUT_D)==1){
+                    DX.DrawBox(60+box_size*2,(H-W)+20+box_size,60+box_size*3,(H-W)+20+box_size*2,DX.GetColor(180,180,180),DX.TRUE);
+                }else{
+                    DX.DrawBox(60+box_size*2,(H-W)+20+box_size,60+box_size*3,(H-W)+20+box_size*2,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD1)==1||DX.CheckHitKey(DX.KEY_INPUT_Z)==1){
+                    DX.DrawBox(20,(H-W)+40+box_size*2,20+box_size,(H-W)+40+box_size*3,DX.GetColor(180,180,180),DX.TRUE);
+                }else{
+                    DX.DrawBox(20,(H-W)+40+box_size*2,20+box_size,(H-W)+40+box_size*3,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD2)==1||DX.CheckHitKey(DX.KEY_INPUT_X)==1){
+                    DX.DrawBox(40+box_size,(H-W)+40+box_size*2,40+box_size*2,(H-W)+40+box_size*3,DX.GetColor(180,180,180),DX.TRUE);
+                }
+                else{
+                    DX.DrawBox(40+box_size,(H-W)+40+box_size*2,40+box_size*2,(H-W)+40+box_size*3,DX.GetColor(50,50,50),DX.TRUE);
+                }
+                if(DX.CheckHitKey(DX.KEY_INPUT_NUMPAD3)==1||DX.CheckHitKey(DX.KEY_INPUT_C)==1){
+                    DX.DrawBox(60+box_size*2,(H-W)+40+box_size*2,60+box_size*3,(H-W)+40+box_size*3,DX.GetColor(180,180,180),DX.TRUE);
+                }else{
+                    DX.DrawBox(60+box_size*2,(H-W)+40+box_size*2,60+box_size*3,(H-W)+40+box_size*3,DX.GetColor(50,50,50),DX.TRUE);
+                }
                 DX.ScreenFlip();
             }
             DX.DxLib_End();
