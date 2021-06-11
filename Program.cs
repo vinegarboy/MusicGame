326a49ca62ba312a98346960e1a7863092740cf4
vinegarboy <necogusa398@gmail.com>
2021-06-11 11:55:49 +0900

    init

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Data;
+using System.Data.Common;
+using System;
 using DxLibDLL;
 using System.Threading;
 using System.IO;
@@ -9,12 +11,21 @@ namespace MusicGame
     {
         static void Main(string[] args)
         {
+            int width = 400,height = 800;
             DX.ChangeWindowMode(DX.TRUE);
-            DX.SetWindowMinSize(400,800);
-            DX.SetWindowMaxSize(400,800);
-            while(DX.CheckHitKey(DX.KEY_INPUT_ESCAPE)!=1){
-                
+            if (DX.DxLib_Init() < 0){
+                return;
             }
+            DX.SetWindowMinSize(width,height);
+            DX.SetWindowMaxSize(width,height);
+            DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+            DX.SetWaitVSyncFlag(DX.FALSE);
+            while(DX.CheckHitKey(DX.KEY_INPUT_ESCAPE)!=1){
+                DX.ClearDrawScreen();
+                //ここからメインループ
+                DX.ScreenFlip();
+            }
+            DX.DxLib_End();
         }
     }
 }
