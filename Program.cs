90d334352f388d146807a73f4681fac102b6bd06
vinegarboy <necogusa398@gmail.com>
2021-06-29 12:39:47 +0900

    四角形の設置＋譜面の設定初期

--- Program.cs
+++ Program.cs
@@ -21,15 +21,17 @@ namespace MusicGame
         static void Main(string[] args)
         {
             int[,] wsizes={{640,480},{800,600},{1024,768},{1280,1024},{1280,720},{1920,1080}};
-            int width = wsizes[0,0],height = wsizes[0,1],cbp = 40,cb_w = width*(cbp/100),cb_h=height*(cbp/100);
+            int width = wsizes[0,0],height = wsizes[0,1],cbp = 40,cb_w = width*cbp/100,cb_h=height*cbp/100;
+            uint white = DX.GetColor(255,255,255);
             //設定
             DX.ChangeWindowMode(1);
             DX.SetWindowSizeChangeEnableFlag( 0,0 ) ;
             DX.DxLib_Init();
             ViewWindow vw = new ViewWindow();
             vw.InitWindowSize(width,height);
+            DX.SetFontSize(cb_w);
             //debugMessage
-            Console.WriteLine($"cb_w{cb_w}  cb_h{cb_h}\n{(width/2)-(cb_w/2)},{(height/2)-(cb_h/2)},{(width/2)+(cb_w/2)},{(height/2)+(cb_h/2)}");
+            Console.WriteLine($"");
             //メインのループ処理
             while(DX.CheckHitKey(DX.KEY_INPUT_ESCAPE)!=1){
                 //画面の更新処理
@@ -37,7 +39,8 @@ namespace MusicGame
                 DX.SetDrawScreen(DX.DX_SCREEN_BACK);
 
                 //ここからメインループ
-                DX.DrawBox(width/2-cb_w/2,height/2-cb_h/2,width/2+cb_w/2,height/2+cb_h/2,DX.GetColor(255,255,255),DX.TRUE);
+                DX.DrawBox(width/2-cb_w/2,height/2-cb_h/2,width/2+cb_w/2,height/2+cb_h/2,white,0);
+                DX.DrawString(width/2-cb_w/2,height/2-cb_h/2,"a",white);
                 DX.ScreenFlip();
             }
             DX.DxLib_End();
