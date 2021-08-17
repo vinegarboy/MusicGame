bffae2a07f78c8a7de5f4d18ee882fad2e3bd4ff
vinegarboy <necogusa398@gmail.com>
2021-08-17 22:43:53 +0900

    仕事からの解放… 2021/8/17 難易度変更機能の作成

--- ViewWindow.cs
+++ ViewWindow.cs
@@ -6,7 +6,7 @@ using DxLibDLL;
 namespace MusicGame
 {
     class ViewWindow{
-        private int width,height,cb_w,cb_h,re = 0,skey = 0,a_x=0,a_y=0,a_w=0,a_h=0;
+        private int width,height,cb_w,cb_h,re = 0,skey = 0,a_x=0,a_y=0,a_w=0,a_h=0,lev_k=0,l_sf=0;
         //Title=0~2 Menu=3~7 Play~ other
         private int[] fontshundle = new int[20];
         private uint white = DX.GetColor(255,255,255);
@@ -42,7 +42,7 @@ namespace MusicGame
             height = nh;
             a_h=height/3;
             a_w=a_h;
-            a_x=(width/2)-(a_w/2);
+            a_x=width/2-(width/20)*4;
             a_y=height/7;
             fontshundle[0] = DX.CreateFontToHandle(null,width/10,10);
             fontshundle[1] = DX.CreateFontToHandle(null,width/12,6);
@@ -61,20 +61,22 @@ namespace MusicGame
             int m_x,m_y;
             DX.DrawStringToHandle(0,0,"All Music",white,fontshundle[1]);
             DX.DrawExtendGraph(a_x,a_y,a_x+a_w,a_y+a_h,md[skey].img,0);
+            l_sf--;
             if(md.Length ==1){
                 DX.DrawStringToHandle(width/2,(height*3)/4,md[skey].title,white,fontshundle[2]);
+                DX.DrawStringToHandle(width/2-(width/20)*4,(height*4)/7,$"Level:{md[skey].levs[lev_k]}",white,fontshundle[2]);
             }
             else if(md.Length ==2){
                 DX.DrawStringToHandle(width/2,(height*3)/4,md[skey].title,white,fontshundle[2]);
                 DX.GetMousePoint(out m_x,out m_y);
-                if((DX.CheckHitKey(DX.KEY_INPUT_RIGHT)==0)||((DX.GetMouseInput()==0)&&(m_x>a_x+a_w))||((DX.GetMouseInput()==0)&&(m_x>a_x+(a_w/2)&&m_x<a_x+a_w)&&(m_y<a_y||m_y>a_y+a_h))){
+                if((DX.CheckHitKey(DX.KEY_INPUT_RIGHT)==1)||((DX.GetMouseInput()==1)&&(m_x>a_x+a_w))||((DX.GetMouseInput()==1)&&(m_x>a_x+(a_w/2)&&m_x<a_x+a_w)&&(m_y<a_y||m_y>a_y+a_h))){
                     //選曲変更
                     if(skey==0){
                         skey=1;
                     }else{
                         skey=0;
                     }
-                }else if((DX.CheckHitKey(DX.KEY_INPUT_LEFT)==0)||((DX.GetMouseInput()==0)&&(m_x<a_x))||((DX.GetMouseInput()==0)&&(m_x>a_x&&m_x<a_x+(a_w/2))&&(m_y<a_y||m_y>a_y+a_h))){
+                }else if((DX.CheckHitKey(DX.KEY_INPUT_LEFT)==1)||((DX.GetMouseInput()==1)&&(m_x<a_x))||((DX.GetMouseInput()==1)&&(m_x>a_x&&m_x<a_x+(a_w/2))&&(m_y<a_y||m_y>a_y+a_h))){
                     //選曲変更
                     if(skey==0){
                         skey=1;
@@ -86,19 +88,33 @@ namespace MusicGame
             else{
                 DX.DrawStringToHandle(width/2,(height*3)/4,md[skey].title,white,fontshundle[2]);
                 DX.GetMousePoint(out m_x,out m_y);
-                if((DX.CheckHitKey(DX.KEY_INPUT_RIGHT)==0)||((DX.GetMouseInput()==0)&&(m_x>a_x+a_w))||((DX.GetMouseInput()==0)&&(m_x>a_x+(a_w/2)&&m_x<a_x+a_w)&&(m_y<a_y||m_y>a_y+a_h))){
+                if((DX.CheckHitKey(DX.KEY_INPUT_RIGHT)==1)||((DX.GetMouseInput()==1)&&(m_x>a_x+a_w))||((DX.GetMouseInput()==1)&&(m_x>a_x+(a_w/2)&&m_x<a_x+a_w)&&(m_y<a_y||m_y>a_y+a_h))){
                     //選曲変更(加算)
                     skey++;
                     if(skey>=md.Length){
                         skey=0;
                     }
-                }else if((DX.CheckHitKey(DX.KEY_INPUT_LEFT)==0)||((DX.GetMouseInput()==0)&&(m_x<a_x))||((DX.GetMouseInput()==0)&&(m_x>a_x&&m_x<a_x+(a_w/2))&&(m_y<a_y||m_y>a_y+a_h))){
+                }else if((DX.CheckHitKey(DX.KEY_INPUT_LEFT)==1)||((DX.GetMouseInput()==1)&&(m_x<a_x))||((DX.GetMouseInput()==1)&&(m_x>a_x&&m_x<a_x+(a_w/2))&&(m_y<a_y||m_y>a_y+a_h))){
                     //選曲変更(減算)
                     skey--;
                     if(skey<0){
                         skey = md.Length-1;
                     }
                 }
+            }
+            if(DX.CheckHitKey(DX.KEY_INPUT_UP)==1&&l_sf<=0){
+                lev_k++;
+                if(lev_k>2){
+                    lev_k=2;
+                }
+                l_sf=30;
+            }
+            else if(DX.CheckHitKey(DX.KEY_INPUT_DOWN)==1&&l_sf<=0){
+                lev_k--;
+                if(lev_k<0){
+                    lev_k=0;
+                }
+                l_sf=30;
             }
             return re;
         }
